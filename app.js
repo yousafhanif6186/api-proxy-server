0996bfaecd44b6d4a10d11fdbab9a2af1d534254
Muhammad yousaf <yousafhanif6186@gmail.com>
2023-12-28 14:22:13 +0500

    proxy server added

---
+++ app.js
@@ -0,0 +1,32 @@
+const http = require('http');
+const httpProxy = require('http-proxy');
+
+// Define the target server to which requests will be proxied
+const target = 'https://api-phonetime.horisen.pro'; // Replace with your target server URL
+
+// Create a proxy server
+const proxy = httpProxy.createProxyServer({});
+
+// Create a regular HTTP server
+const server = http.createServer((req, res) => {
+    // Log the incoming request URL for demonstration purposes
+    console.log(`Request received for: ${req.url}`);
+
+    // Proxy the request to the target server
+    proxy.web(req, res, { target });
+});
+
+// Handle proxy errors
+proxy.on('error', (err, req, res) => {
+    console.error('Proxy Error:', err);
+    res.writeHead(500, {
+        'Content-Type': 'text/plain',
+    });
+    res.end('Proxy Error');
+});
+
+// Listen on a specific port
+const port = 3004; // Replace with your desired port
+server.listen(port, () => {
+    console.log(`Proxy server is listening on port ${port}`);
+});
